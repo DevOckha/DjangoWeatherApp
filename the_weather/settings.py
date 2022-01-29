9bfb788f17f160b88627294a520504b0af736226
AkaOckha <yakopl@hotmail.com>
2022-01-29 14:08:27 +0300

    editing settings

--- the_weather/settings.py
+++ the_weather/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u3mikv%v2dcg#lq%^-etj*ek7zwo+is$sz%47mthj(u6xdrv1&'
+#SECRET_KEY = 'django-insecure-u3mikv%v2dcg#lq%^-etj*ek7zwo+is$sz%47mthj(u6xdrv1&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['wweatheraapp.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
